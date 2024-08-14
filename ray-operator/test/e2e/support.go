eaf99c9911ce754a215471f6c028e48d9f61549a
Karel Suta <ksuta@redhat.com>
2024-08-14 10:18:37 +0200

    PATCH: Raise head pod memory limit to avoid test instability

--- ray-operator/test/e2e/support.go
+++ ray-operator/test/e2e/support.go
@@ -138,7 +138,7 @@ func headPodTemplateApplyConfiguration() *corev1ac.PodTemplateSpecApplyConfigura
 					}).
 					WithLimits(corev1.ResourceList{
 						corev1.ResourceCPU:    resource.MustParse("500m"),
-						corev1.ResourceMemory: resource.MustParse("2G"),
+						corev1.ResourceMemory: resource.MustParse("3G"),
 					}))))
 }
 
